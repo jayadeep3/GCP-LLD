fb2240e8dfd92af61be8654366a21af4212d586e
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-10-08 15:32:22 +0530

    Add files via upload

---
+++ GCP/env/dev_prj/security/variables.tf
@@ -0,0 +1,24 @@
+variable "kms_environments" {
+  description = "Map of environment configurations for KMS keys"
+  type = map(object({
+    region           = string
+    key_ring         = string
+    key_name         = string
+    algorithm        = string
+    protection_level = string
+    rotation_days    = number
+    labels           = map(string)
+    # iam_bindings     = list(object({
+    #   role   = string
+    #   member = string
+    # }))
+  }))
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
