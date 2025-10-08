f93360f752dbebfac5e4224fa800a698ca78a03a
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-10-08 15:31:45 +0530

    Create main.tf

---
+++ GCP/env/dev_prj/security/main.tf
@@ -0,0 +1,18 @@
+locals {
+  kms_configs = var.kms_environments
+}
+
+module "kms" {
+  for_each = local.kms_configs
+
+  source   = "../../../modules/security/cmek_keys"
+
+  project_id          = var.project_id
+  region              = each.value.region
+  key_ring_name       = each.value.key_ring
+  key_name            = each.value.key_name
+  algorithm           = each.value.algorithm
+  protection_level    = each.value.protection_level
+  rotation_days       = each.value.rotation_days
+  labels              = each.value.labels 
+}
